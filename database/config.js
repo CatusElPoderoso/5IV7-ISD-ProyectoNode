9bb5054edaff0703fbe8a5b580f69843d51087d2
Ariadna <achicol1900@alumno.ipn.mx>
2021-12-01 15:06:25 -0600

    configuración de la base de datos

--- database/config.js
+++ database/config.js
@@ -1,14 +1,18 @@
 // imports
 const mongoose = require('mongoose');
 
-// conexión a la base 
+/** 
+ * aquí se hace la conexión a la base de datos de  
+ * mongoose, para que los usuarios puedan hacer uso correcto del CRUD
+ */
+
 const connection = async() => {
-    try {
+     try{
         await mongoose.connect(process.env.MONGODB_CNN); // intenta la conexión con mongoose
-        console.log(`Base de datos iniciada`); // se conectó la base
-   }catch (err) {
+        console.log(`Base de datos iniciada correctamente`); // se conectó la base
+     }catch (err) {
         throw new Error(`Hubo un error de conexión con la base de datos: \n${err}`); // la base no se pudo conectar D:
-   };
+     };
 };
 
 // exports
